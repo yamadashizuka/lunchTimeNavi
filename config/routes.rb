e793ad15f0a14b2c9c6e709166eb2ca7458652d4
sankai <ksankai@hotmail.com>
2012-11-09 00:11:49 +0900

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -69,7 +69,7 @@ LunchTimeNavi::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
   match 'lunch_comments/new2/:id' => 'lunch_comments#new2'
   match 'lunches/new2/:id' => 'lunches#new2'
 end
