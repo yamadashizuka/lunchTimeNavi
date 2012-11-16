fa25ecd6be5bcd66037d1e4d02023715abe59b6c
Yoshini Shioi <ys_senri_2003@mx5.canvas.ne.jp>
2012-11-16 23:44:31 +0900

    I replaced text_filed helper to telephone_field helper at telephone. I replaced text_filed helper to url_field hepler at url. I replaced text_filed helper to email_field at email. I added minuts option to time_field.
    Signed-off-by: Yoshini Shioi <shioi_yoshini@ogis-ri.co.jp>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121108163035) do
+ActiveRecord::Schema.define(:version => 20121109143151) do
 
   create_table "genres", :force => true do |t|
     t.string   "name"
