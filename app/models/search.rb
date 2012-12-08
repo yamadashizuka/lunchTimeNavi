40e0f10d30d1d8cb5e340801bd9c8ce824579055
Yoshini Shioi <ys_senri_2003@mx5.canvas.ne.jp>
2012-12-08 21:43:11 +0900

    _form.html.erb に統一 ratingの絞込み不具合対応 index.html.erbとsearch.html.erbの表示整合 ページネーションをするなら、独自実装か、テーブルビュー方式か、 2方式あり。でも、そこにはまだ手をつけていない
    Signed-off-by: Yoshini Shioi <shioi_yoshini@ogis-ri.co.jp>

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,12 +2,12 @@ class Search
 
   attr_accessor :restaurantName, :address, :genre_id, :rating_id, :reservation
 
-  def initialize(restaurantName, address, genre_id, rating_id, reservation)
-    @restaurantName = restaurantName
-    @address        = address
-    @genre_id       = genre_id
-    @rating_id      = rating_id
-    @reservation    = reservation
+  def initialize(params)
+    @restaurantName = params[:restaurantName]
+    @address        = params[:address]
+    @genre_id       = params[:genre_id]
+    @rating_id      = params[:rating_id]
+    @reservation    = params[:reservation]
   end
   
 end
